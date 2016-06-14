6a4726004d652ca1edce6eea20712e8fad66a40f
Bob Sakson <bobsakson@Bobs-MacBook-Pro.local>
2016-06-13 19:03:06 -0500

    Boilerplate code with ExpressJS

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+var indexRoutes = require('./routes/index.js')
+app.use('/', indexRoutes);
+
+//app.set('views', __dirname);
+app.set('view engine', 'pug');
+
+app.listen(3000, function () {
+  console.log('App running on port 3000.');
+});
