69344358e662d9393d6c287edc6899ca602f454e
bobsakson <bob.sakson@hotmail.com>
2016-06-13 22:02:29 -0500

    Added nodemon and browser-sync. Need to add views as watched files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,42 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
+var nodemon = require('gulp-nodemon');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task('default', function() {
-  exec('node index.js');
+gulp.task('scripts', function() {
+    return gulp.src('./*.js', './routes/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('scripts-watch', ['scripts'], reload);
+
+gulp.task('nodemon', function (cb) {
+    var called = false;
+
+    return nodemon({
+            script: 'index.js',
+            ext: 'js',
+            ignore: ['public/*', 'views/*']
+    })
+        .on('start', function () {
+            if (!called) {
+                called = true;
+                cb();
+        }
+    });
+});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+    browserSync.init(null, {
+        proxy: "http://localhost:3000",
+        browser: ['google chrome'],
+        port: 4000
+    });
+});
+
+gulp.task('default', ['browser-sync'], function() {
+  //exec('node index.js');
+  gulp.watch(['./*.js', './routes/*.js'], ['scripts-watch']);
 });
