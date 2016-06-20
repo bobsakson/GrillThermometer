ebf2d72058056fc39c322a4567a5eeda4de48f74
bobsakson <mckybob@gmail.com>
2016-06-20 18:07:42 -0500

    Added polling code for the MCP3008

--- services/index.js
+++ services/index.js
@@ -1,23 +1,41 @@
 var dnode = require('dnode');
 var sqlite3 = require('sqlite3');
+var mcp3008 = require('mcp3008.js');
 
 var db = new sqlite3.Database('../thermometer.db');
 var intervalObject = null;
+var adc = new mcp3008();
 
 dnode(function (client) {
     this.startProbe = function (cb) {
         client.poll(function () {
             console.log('Probe - Start');
 
-            currentTemp = 40;
-            iteration = 1;
+            // currentTemp = 40;
+            // iteration = 1;
 
-            intervalObject = setInterval(function() {
-                if((iteration % 10) === 0) {
-                    currentTemp++;
-                    iteration = 1;
-                }
+            // intervalObject = setInterval(function() {
+            //     if((iteration % 10) === 0) {
+            //         currentTemp++;
+            //         iteration = 1;
+            //     }
 
+            //     var currentdate = new Date();
+            //     var readingDateTime =   (currentdate.getMonth()+1) + "/"
+            //                             + currentdate.getDate() + "/" 
+            //                             + currentdate.getFullYear() + " "  
+            //                             + currentdate.getHours() + ":"  
+            //                             + currentdate.getMinutes() + ":" 
+            //                             + currentdate.getSeconds();
+
+            //     db.run('INSERT INTO temperaturelog VALUES($temperature, $readingDateTime)', { $temperature: currentTemp, $readingDateTime: readingDateTime});
+
+            //     iteration++;
+            //     cb(currentTemp);
+            // }, 1000);
+
+            adc.poll(0, 1000, function(currentTemp) {
+               
                 var currentdate = new Date();
                 var readingDateTime =   (currentdate.getMonth()+1) + "/"
                                         + currentdate.getDate() + "/" 
@@ -25,19 +43,21 @@ dnode(function (client) {
                                         + currentdate.getHours() + ":"  
                                         + currentdate.getMinutes() + ":" 
                                         + currentdate.getSeconds();
-
+                
+                console.log('Reading ', currentTemp);
                 db.run('INSERT INTO temperaturelog VALUES($temperature, $readingDateTime)', { $temperature: currentTemp, $readingDateTime: readingDateTime});
 
-                iteration++;
                 cb(currentTemp);
-            }, 1000);
+            });
         });
     }; 
 
     this.stopProbe = function (cb) {
         console.log('Probe - Stop');
 
-        clearInterval(intervalObject);
+        //clearInterval(intervalObject);
+        adc.stop();
+        adc.close();
         cb();
     };
 }).listen(6060);
