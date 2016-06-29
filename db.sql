87b8a168bbcaa68ecd6667134557d0c8f90fd01c
bobsakson <mckybob@gmail.com>
2016-06-28 21:52:51 -0500

    Working on the profile repo

--- db.sql
+++ db.sql
@@ -1,6 +1,8 @@
 PRAGMA foreign_keys = ON;
 CREATE TABLE probes(channel INTEGER PRIMARY KEY, label TEXT);
 CREATE TABLE temperaturelog(channel INTEGER REFERENCES probes(channel), fahrenheit REAL, celsius REAL, kelvin REAL, readingDateTime DEFAULT CURRENT_TIMESTAMP);
+CREATE TABLE profile(id INTEGER PRIMARY KEY, name TEXT, description TEXT, isDeleted BOOLEAN);
+CREATE TABLE probeProfile(id INTEGER PRIMARY KEY, channel INTEGER REFERENCES probes(channel), profileId INTEGER REFERENCES profile(id), label TEXT, upperThreshold INTEGER, lowerThreshold INTEGER, isDeleted BOOLEAN);
 
 -- Below is test data.
 INSERT INTO probes VALUES (0, 'Food');
--- web/dal/profilesRepo.js
+++ web/dal/profilesRepo.js
@@ -1,11 +1,15 @@
 var sqlite3 = require('sqlite3');
 
 var connectToDatabase = function() {
-    var db = new sqlite3.Database('../../thermometer.db');
+    return new sqlite3.Database('../../thermometer.db');
 }
 
 var getProfiles = function() {
+    var db = connectToDatabase();
+
     db.all('SELECT id, name, description FROM profile WHERE isDeleted = 0', function(err, rows) {
+        db.close();
+
         if(err) {
             console.log(err);
         }
@@ -16,8 +20,11 @@ var getProfiles = function() {
 };
 
 var getProfile = function(id) {
+    var db = connectToDatabase();
+
     db.all('SELECT p.id, p.name, p.description, pp.id, pp.channel, pp.label, pp.upperThreshold, pp.lowerThreshold FROM profile p INNER JOIN profileProbe pp ON p.id = pp.profileId WHERE p.id = $id AND pp.isDeleted = 0', { $id: id }, function(err, rows) {
         if(err) {
+            db.close();
             console.log(err);
         }
         else {
@@ -37,15 +44,38 @@ var getProfile = function(id) {
 
                     profile.probes.push(probe);
                 }, this);
+
+                db.close();
             }
         }
     });
 };
 
 var saveProfile = function(profile) {
-    
+    var db = connectToDatabase();
+
+    // TODO: Need to convert this to a transaction.
+    db.run('INSERT INTO profile(name, description, isDeleted) VALUES($id, $name, $description, $isDeleted)', { $name: profile.name, $description: profile.description, $isDeleted: 0 }, function(err, row) {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            db.run('SELECT MAX(id) as maxId FROM profile', function(err, row) {
+                profile.probes.forEach(function(probe) {
+                    db.run('INSERT INTO probeProfile(channel, profileId, label, upperThreshold, lowerThreshold, isDeleted) VALUES($channel, $profileId, $label, $upperThreshold, $lowerThreshold, $isDeleted)', { $channel: probe.channel, $profileId: row.maxId, $label: probe.label, $upperThreshold: probe.upperThreshold, $lowerThreshold: probe.lowerThreshold, $isDeleted: 0});
+                }, this);
+            });
+        }
+    });
+};
+
+var updateProfile = function(profile) {
+    var db = connectToDatabase();
+
+    db.close();
 };
 
 module.exports.getProfiles = getProfiles;
 module.exports.getProfile = getProfile;
 module.exports.saveProfile = saveProfile;
+module.exports.updateProfile = updateProfile;
